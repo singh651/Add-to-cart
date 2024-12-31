0be222949033e10776898045597633999bbcb0ea
Parikshit Singh <65848309+singh651@users.noreply.github.com>
2024-12-31 13:15:32 +0000

    Commiting

---
+++ script.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const products = [
+        {id: 1, name: "Product 1", price: 29.99},
+        {id: 2, name: "Product 2", price: 19.99},
+        {id: 3, name: "Product 3", price: 59.99},
+    ];
+
+    const cart = [];
+    const ProductList = document.getElementById("product-list")
+    
+})
